421cdc196aceed946e2971e7493fc373d6874648
Eugene Kuzmenko <aljey1972@gmail.com>
2015-09-30 15:50:52 +0300

    [fixed] only run tests after a successful build

--- bin/build.js
+++ bin/build.js
@@ -10,7 +10,8 @@ var rootDir = join(__dirname, '..'),
     specDir = join(rootDir, 'spec'),
     readme = join(rootDir, 'README.md'),
     js = new JS(),
-    jsdoc = new NativeProcess(join(rootDir, 'node_modules', '.bin', 'jsdoc'));
+    jsdoc = new NativeProcess(join(rootDir, 'node_modules', '.bin', 'jsdoc')),
+    npm = new NativeProcess('npm');
 
 js.beDir(libDir, buildDir, function () {
   jsdoc.run(function (e) {
@@ -18,5 +19,6 @@ js.beDir(libDir, buildDir, function () {
       return console.error(e);
     }
     console.log('\x1b[32mGenerated API documentation!\x1b[0m');
+    npm.run(Function.prototype, ['test'], {stdio: 'inherit'});
   }, [buildDir, '-d', docsDir, '-R', readme]);
 }, specDir, __filename);
